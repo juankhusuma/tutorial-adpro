4726e050bacde615842b9acc0ca2636d758950ac
juankhusuma <66712975+juankhusuma@users.noreply.github.com>
2024-03-03 20:44:18 +0700

    [RED] Add Order model skeleton.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -6,11 +6,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 public class Order {
     private String id;
     private List<Product> products;
     private long orderTime;
     private String author;
+    @Setter
     private String status;
+
+    public Order(String id, List<Product> products, Long orderTime, String author) {
+    }
+
+    public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+    }
 }
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -35,7 +35,6 @@ public class OrderTest {
         assertThrows(IllegalArgumentException.class, () -> {
             Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                     this.products, 1708560000L, "Safira Sudrajat");
-            order.setProducts(this.products);
         });
     }
 
