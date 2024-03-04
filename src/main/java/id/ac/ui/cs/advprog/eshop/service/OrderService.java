d14be2fe3394b6b10c61e705d8e436140d7da185
juankhusuma <66712975+juankhusuma@users.noreply.github.com>
2024-03-05 01:50:09 +0700

    [RED] Add OrderServiceImpl skeleton.

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderService.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface OrderService {
+    Order createOrder(Order order);
+
+    Order updateStatus(String orderId, String status);
+
+    Order findById(String orderId);
+
+    List<Order> findAllByAuthor(String id);
+}
